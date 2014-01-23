1ca67b443ed2062326bf26fb32d744c97ef83f1e
Emka0611 <emilia.chodorowska0611@gmail.com>
2014-01-23 22:36:20 +0100

    szablonowanie init

--- CashSaver/src/com/example/cashsaver/SectionFragmentShoppingLists.java
+++ CashSaver/src/com/example/cashsaver/SectionFragmentShoppingLists.java
@@ -1,10 +1,12 @@
 package com.example.cashsaver;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class SectionFragmentShoppingLists extends Fragment
 {
@@ -12,6 +14,18 @@ public class SectionFragmentShoppingLists extends Fragment
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
 		View rootView = inflater.inflate(R.layout.fragment_start_lists, container, false);
+		
+		Button btn = (Button)rootView.findViewById(R.id.button);
+		btn.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View arg0)
+			{
+				Intent i = new Intent(getActivity(), ShoppingListsActivity.class);
+				getActivity().startActivity(i);	
+			}
+		});
+		
 		return rootView;
 	}
 
