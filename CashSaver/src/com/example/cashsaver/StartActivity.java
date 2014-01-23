e54388120f3efa30291a02ac1c140057f4ebd7cc
Emka0611 <emilia.chodorowska0611@gmail.com>
2014-01-23 02:21:24 +0100

    srollable tabs

--- CashSaver/src/com/example/cashsaver/StartActivity.java
+++ CashSaver/src/com/example/cashsaver/StartActivity.java
@@ -1,23 +1,13 @@
 package com.example.cashsaver;
 
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
 
-public class StartActivity extends FragmentActivity implements ActionBar.TabListener
+public class StartActivity extends FragmentActivity
 {
-
-	/* PagerAdapter will provide fragments for each of the sections.
-	 * We use a FragmentPagerAdapter derivative, which
-	 * will keep every loaded fragment in memory.
-	 * If this becomes too memory intensive,
-	 * it may be best to switch to a FragmentStatePagerAdapter*/
 	SectionsPagerAdapter mSectionsPagerAdapter;
-
-	//ViewPager will host the section contents.
 	ViewPager mViewPager;
 
 	@Override
@@ -26,33 +16,10 @@ public class StartActivity extends FragmentActivity implements ActionBar.TabList
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start);
 
-		final ActionBar actionBar = getActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-		// Create the adapter that will return a fragment for each of the primary sections
 		mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), this);
 
-		// Set up the ViewPager with the sections adapter.
 		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
-
-		// When swiping between different sections, select the corresponding tab.
-		// We can also use ActionBar.Tab#select() to do this if we have a reference to the Tab.
-		mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener()
-		{
-			@Override
-			public void onPageSelected(int position)
-			{
-				actionBar.setSelectedNavigationItem(position);
-			}
-		});
-
-		// For each of the sections in the app, add a tab to the action bar.
-		for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++)
-		{
-			actionBar.addTab(actionBar.newTab().setText(mSectionsPagerAdapter.getPageTitle(i)).setTabListener(this));
-		}
-		
+		mViewPager.setAdapter(mSectionsPagerAdapter);		
 	}
 
 	@Override
@@ -60,21 +27,5 @@ public class StartActivity extends FragmentActivity implements ActionBar.TabList
 	{
 		getMenuInflater().inflate(R.menu.start, menu);
 		return true;
-	}
-
-	@Override
-	public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
-	{
-		mViewPager.setCurrentItem(tab.getPosition());
-	}
-
-	@Override
-	public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
-	{
-	}
-
-	@Override
-	public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
-	{
 	}
 }
