ef3f2ffdc1414c4ade50c64ed22ceaeb911c720e
Emka0611 <emilia.chodorowska0611@gmail.com>
2014-01-23 22:36:47 +0100

    szablonowanie init

---
+++ CashSaver/src/com/example/cashsaver/ShoppingListsActivity.java
@@ -0,0 +1,14 @@
+package com.example.cashsaver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShoppingListsActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_shopping_list);
+	}
+
+}
