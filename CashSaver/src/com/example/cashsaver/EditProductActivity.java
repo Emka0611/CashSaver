b8c0624c76afb2a6168aaa7fb0b0c142cb0f20bb
Emka0611 <emilia.chodorowska0611@gmail.com>
2014-01-29 23:16:51 +0100

    edit form part 2

--- CashSaver/src/com/example/cashsaver/EditProductActivity.java
+++ CashSaver/src/com/example/cashsaver/EditProductActivity.java
@@ -1,5 +1,8 @@
 //TODO: zgubi³ titla po on resume
 //TODO: osobny layout dla poziomego widoku
+//TODO: porzadek tutaj
+//TODO: search layout
+
 package com.example.cashsaver;
 
 import android.app.Activity;
@@ -24,6 +27,15 @@ public class EditProductActivity extends Activity
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		// Apply the adapter to the spinner
 		spinner.setAdapter(adapter);
+		
+		Spinner spinner2= (Spinner) findViewById(R.id.spinner2);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+		        R.array.units_array, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner2.setAdapter(adapter2);
 		
 	}
 
